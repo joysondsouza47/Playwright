4dd00287bf641060744d58a11e642080f95b516d
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-09 08:02:40 +0530

    1st commit

--- tests/demotest.spec.ts
+++ tests/demotest.spec.ts
@@ -1,29 +1,55 @@
-import {test, expect, Locator} from "@playwright/test"
+import{test,expect,Locator} from "@playwright/test"
+
+// getByText
+// getByAltText
+// getByLabel
+// getByPlaceholder
+// getByTestId
+// getByRole
+// getByTitle
+ 
+// page.getByRole()        Locate elements by accessibility roles like button, checkbox, heading, etc. 
+// page.getByText()        Locate by visible text content. 
+// page.getByLabel()       Locate form controls using associated label text. 
+// page.getByPlaceholder() Locate inputs via placeholder text. 
+// page.getByAltText()     Locate images by their alternative text (alt attribute). 
+// page.getByTitle()       Locate elements by their title attribute. 
+// page.getByTestId()      Locate by a custom data attribute like data-testid.
 
 
-test("demo locator TITLE", async({page})=>{
-await page.goto("https://www.saucedemo.com/");
-const title:string = await page.title();
-console.log(title);
-await expect(page).toHaveTitle("Swag Labs");
-await expect(page.getByText("Swag Labs")).toBeVisible();
+test("demo title", async({page})=>{
 
+await page.goto("https://demowebshop.tricentis.com/");
 
-await page.getByPlaceholder("Username").fill("standard_user");
-await page.getByPlaceholder("Password").fill("secret_sauce");
+// page.getByRole() 
+await expect(page.getByRole('heading',{name: "Welcome to our store"})).toBeVisible();
 
-await page.getByTestId("login-button").click();
+// page.getByText()
+await expect(page.getByText("Welcome to our store")).toBeVisible();
 
-await expect(page.getByAltText("Sauce Labs Backpack")).toBeVisible();
-await expect(page.getByText(/29.99/)).toBeVisible();
+// page.getByLabel()
+await page.getByLabel("Excellent").click();
 
-await page.getByTestId("add-to-cart-sauce-labs-backpack").click();
-await page.getByTestId("shopping-cart-link").click();
-await page.getByTestId("shopping-cart-link").click();
+// page.getByAltText()
+await expect(page.getByAltText("Tricentis Demo Web Shop")).toBeVisible();
 
+// page.getByTitle()
+const titlepage:string = await page.title();
+console.log(titlepage);
+await expect(page.getByTitle("Demo Web Shop")).toBeHidden();
 
-await page.getByText
 
 })
 
+test("second test", async({page})=>{
 
+await page.goto("https://www.saucedemo.com/");
+
+// page.getByPlaceholder()
+await page.getByPlaceholder("Username").fill("standard_user")
+await page.getByPlaceholder("Password").fill("secret_sauce")
+
+// page.getByTestId()
+await page.getByTestId("login-button").click();
+
+})
