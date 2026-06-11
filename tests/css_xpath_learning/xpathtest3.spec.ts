af87f978847702cdce01b5cf9b1ded0dec6ac06d
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-11 19:37:30 +0530

    11-06-2026

--- tests/css_xpath_learning/xpathtest3.spec.ts
+++ tests/css_xpath_learning/xpathtest3.spec.ts
@@ -20,5 +20,34 @@ test("test 1" , async({page})=> {
 
     console.log(await coulms.textContent());
 
-    // 
+    // ancestor
+    const ancestor:Locator = page.locator("//td[text()='Francisco Chang']/ancestor::table");
+
+    await expect(ancestor).toHaveAttribute('id','customers');
+
+    // descendant
+
+    await expect(page.locator("//table[@id='customers']/descendant::th[2]")).toHaveText("Contact");
+
+
+    // following
+
+    await expect(page.locator("//td[normalize-space()='Germany']/following::td[3]")).toHaveText("Mexico");
+
+    // following-sibling
+
+    await expect(page.locator("//td[normalize-space()='Alfreds Futterkiste']/following-sibling::td")).toHaveCount(2);
+
+    // preceding
+
+    const predceding:Locator = page.locator("//td[normalize-space()='Mexico']/preceding::td");
+
+    await expect(predceding).toHaveCount(5);
+
+    console.log(await predceding.allTextContents());
+
+    // preceding-sibling
+
+    await expect(page.locator("//td[normalize-space()='Canada']/preceding-sibling::td")).toHaveCount(2);
+
 })
