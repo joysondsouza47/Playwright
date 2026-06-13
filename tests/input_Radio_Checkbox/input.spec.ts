eb3c756db2fc66cdcc5110f73be51ee9d59b6a66
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-13 13:11:21 +0530

    13_06_2026_1

--- tests/input_Radio_Checkbox/input.spec.ts
+++ tests/input_Radio_Checkbox/input.spec.ts
@@ -2,16 +2,52 @@ import {test, expect, Locator} from "@playwright/test"
 
 
 
-test("test title", async ({page})=>{
+test("input test", async ({page})=>{
 
    await page.goto("https://testautomationpractice.blogspot.com/");
 
-   await page.getByPlaceholder("Enter Name").fill("joyson");
-   await page.getByPlaceholder("Enter EMail").fill("joysondsouza@gmail.com");
-   await page.getByPlaceholder("Enter Phone").fill("9945636877");
-   await page.locator("#textarea").fill("area")
+   const inputname:Locator = page.locator("#name");
 
+   await expect(inputname).toBeVisible();
+
+   await expect(inputname).toBeEnabled();
+
+   await expect(inputname).not.toBeDisabled();
+
+   const attributevalue = await inputname.getAttribute("maxlength");
+
+   expect(attributevalue).toBe("15");
+
+   await inputname.fill("joyson dsouza");
+
+   const inputtext = await inputname.inputValue();   //inputValue = value given to input field   // innerText = text value already present in the element
+
+   console.log(inputtext);
+
+   expect(inputtext).toBe("joyson dsouza")
 
    await page.waitForTimeout(3000);
+
+})  //.only runs only the mentioned test
+
+test.only("Radio test",async({page})=>{
+
+await page.goto("https://testautomationpractice.blogspot.com/")
+
+const inputname:Locator = page.locator("#name");
+await inputname.fill("joyson dsouza");
+const radiomalecheck:Locator = page.locator("#male");
+await expect(radiomalecheck).toBeVisible();
+await expect(radiomalecheck).toBeEnabled();
+await expect(radiomalecheck).not.toBeDisabled();
+await radiomalecheck.check();
+await expect(radiomalecheck).toBeChecked();
+
+// const malebuttonchecked:boolean= await radiomalecheck.isChecked();
+// console.log(malebuttonchecked);
+// expect(malebuttonchecked).toBe(true)
+
+expect(await radiomalecheck.isChecked()).toBe(true);
+
 
 })
