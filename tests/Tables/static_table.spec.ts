1d53d32d6906c21337e52183a8522cfc6cbfd233
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-17 20:29:27 +0530

    17_06_2026_3

--- tests/Tables/static_table.spec.ts
+++ tests/Tables/static_table.spec.ts
@@ -5,6 +5,27 @@ test("Table title",async({page})=>{
 
     await page.goto("https://testautomationpractice.blogspot.com/");
 
+    const table:Locator = page.locator("table[name=BookTable]>tbody");
+
+    const rows:Locator = table.locator("tr");
+
+    console.log(await rows.allInnerTexts());
+
+    const rowscount:number= await rows.count();
+
+    console.log(rowscount);
+
+    const columns:Locator = rows.nth(1).locator("td");
+
+    const columncount:number = await columns.count();
+
+    console.log(columncount);
+
+    console.log(await columns.allInnerTexts());
+
+
+
+
 
 
 
