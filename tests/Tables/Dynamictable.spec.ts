d2a7fe210b2aaa73819326900fcca0ef9ba0d3d9
joysondsouza47 <joysondsouza47@gmail.com>
2026-06-23 10:26:05 +0530

    23_06_2026_2

--- tests/Tables/Dynamictable.spec.ts
+++ tests/Tables/Dynamictable.spec.ts
@@ -13,26 +13,42 @@ test("dynamic table", async({page})=> {
     console.log(await rows[1].allInnerTexts());
 
 
-let cpuvalue ;
-    for(const row of rows)
-{
-        const name:string = await row.locator("td").nth(0).innerText();
+let CPUvalue;
+let Diskvalue;
+let Netwrokvalue;
+let Memoryvalue;
 
-        if(name=='Chrome')
+for(const row of rows)
+
+    {
+        const name:string = await row.locator("td").nth(0).innerText()
+
+        if(name==='Chrome')
         {
-            cpuvalue = await row.locator('td:has-text("%")').innerText();
-            console.log(cpuvalue);
-            break;
+            CPUvalue = await row.locator("td:has-text('%')").innerText();
+            Netwrokvalue = await row.locator("td:has-text('Mbps')").innerText();
         }
+        else if(name ==='Firefox')
+        {
+            Memoryvalue = await row.getByText(/MB$/).innerText();
+            Diskvalue = await row.locator("td:has-text('MB/s')").innerText();
+        }
+    }
 
-}
+const CPUwebvalue:string = await page.locator("strong.chrome-cpu").innerText();
+const Diskwebvalue:string = await page.locator("strong.firefox-disk").innerText();
+const Netwrokwebvalue:string = await page.locator("strong.chrome-network").innerText();
+const Memorywebvalue:string = await page.locator("strong.firefox-memory").innerText();
 
+console.log("CPU value: ",CPUvalue , "\nCPU web value: " ,CPUwebvalue);
+console.log("Disk value: ",Diskvalue , "\nDisk web value: " ,Diskwebvalue);
+console.log("Netwrok value: ",Netwrokvalue , "\nNetwrok web value: " ,Netwrokwebvalue);
+console.log("Memory value: ",Memoryvalue , "\nMemory web value: " ,Memorywebvalue);
 
- const cpuwebvalue:string = await page.locator("strong.chrome-cpu").innerText();
-
- expect(cpuvalue === cpuwebvalue);
-  console.log("Cpu table value:",cpuvalue);
-  console.log("cpu web value:",cpuwebvalue);
+expect(CPUvalue).toContain(CPUwebvalue);
+expect(Diskvalue).toContain(Diskwebvalue);
+expect(Netwrokvalue).toContain(Netwrokwebvalue);
+expect(Memoryvalue).toContain(Memorywebvalue);
 
 
 })
